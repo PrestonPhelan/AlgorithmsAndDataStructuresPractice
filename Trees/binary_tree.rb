2b757482294f641865a742092ad899d1a05514fb
PrestonPhelan <preston.phelan@gmail.com>
2017-08-05 16:17:31 -0700

    BinTree class, traverseInOrder methods

---
+++ Trees/binary_tree.rb
@@ -0,0 +1,28 @@
+require('tree')
+
+class BinTree < Tree
+  def traverseInOrder
+    @root.traverseInOrder
+  end
+end
+
+class BinTreeNode < TreeNode
+  attr_accessor :left_child, :right_child
+
+  def initialize(value, parent = nil, children = [])
+    super(value, parent, children)
+    @left_child = children.first
+    @right_child = children.last
+  end
+
+  def traverseInOrder
+    @left_child.traverseInOrder if @left_child
+    self.visit
+    @right_child.traverseInOrder if @right_child
+  end
+
+  def visit
+    # Placeholder for visiting a node
+    puts @value
+  end
+end
