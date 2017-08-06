d4c03db9d900a96b2c237ff8866109ae590f7b57
PrestonPhelan <preston.phelan@gmail.com>
2017-08-06 15:50:50 -0700

    Graph bidirectional search

--- Graphs/graph.rb
+++ Graphs/graph.rb
@@ -32,6 +32,42 @@ class Graph
 
     nil
   end
+
+  def bidirectional_search(node1, node2)
+    # Track number of steps taken in each search
+    steps1 = 0
+    steps2 = 0
+
+    visited1 = Set.new
+    visited2 = Set.new
+
+    node_queue1 = [node1]
+    node_queue2 = [node2]
+
+    until node_queue1.empty? && node_queue2.empty?
+      current_node = node_queue1.shift
+      return steps1 + steps2 if visited2.include?(current_node)
+
+      visited1 << current_node
+      current_node.adjacents.each do |adj_node|
+        node_queue1 << adj_node unless visited1.include?(adj_node)
+      end
+
+      steps1 += 1
+
+      current_node = node_queue2.shift
+      return steps1 + steps2 if visited1.include?(current_node)
+
+      visited2 << current_node
+      current_node.adjacents.each do |adj_node|
+        node_queue2 << adj_node unless visited2.include?(adj_node)
+      end
+
+      steps2 += 1
+    end
+
+    nil
+  end
 end
 
 class GraphNode
