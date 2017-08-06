2072a8643e6769621a4eb3ef39367600b0849912
PrestonPhelan <preston.phelan@gmail.com>
2017-08-06 16:40:45 -0700

    In place quicksort

--- Sorts/quick_sort.rb
+++ Sorts/quick_sort.rb
@@ -18,3 +18,40 @@ def quick_sort(array, &prc)
 
   quick_sort(left, &prc) + [part_val] + quick_sort(right, &prc)
 end
+
+def quick_sort_in_place!(array, start = 0, finish = array.length - 1, &prc)
+  return if array.length <= 1 || start >= finish
+  prc ||= Proc.new { |el1, el2| el1 - el2 }
+
+  part_idx = start + ((finish - start + 1) / 2)
+
+  array[part_idx], array[finish] = array[finish], array[part_idx]
+  pivot = array[finish]
+
+  i = start #tracks number of left-side values
+  j = start #tracks index currently checking
+
+  until j == finish
+    if prc.call(array[j], pivot) < 0
+      array[i], array[j] = array[j], array[i]
+      i += 1
+    end
+    j += 1
+  end
+
+  array[i], array[j] = array[j], array[i]
+
+  quick_sort_in_place!(array, start, i - 1, &prc)
+  quick_sort_in_place!(array, i + 1, finish, &prc)
+
+  array
+end
+
+# a = Array.new(8) { rand(1...9) }
+# print a
+# puts ""
+#
+# quick_sort_in_place!(a)
+#
+# print a
+# puts ""
