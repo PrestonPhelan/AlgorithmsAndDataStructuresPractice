460650b4435f0a114733e2c0695b605f262a68b9
PrestonPhelan <preston.phelan@gmail.com>
2017-08-06 16:04:38 -0700

    Merge sort with ability to accept custom sort conditions

--- Sorts/merge_sort.rb
+++ Sorts/merge_sort.rb
@@ -1,16 +1,18 @@
 # Merge Sort
 # Runtime: O(n log(n)), Memory: O(n)
 
-def merge_sort(array)
+def merge_sort(array, &prc)
   return array if array.length <= 1
 
+  prc ||= Proc.new { |el1, el2| el1 - el2 }
+
   mid_idx = array.length / 2
-  sorted_left = merge_sort(array[0...mid_idx])
-  sorted_right = merge_sort(array[mid_idx..-1])
+  sorted_left = merge_sort(array[0...mid_idx], prc)
+  sorted_right = merge_sort(array[mid_idx..-1], prc)
 
   sorted = []
   until sorted_left.empty? || sorted_right.empty?
-    if sorted_left.first <= sorted_right.first
+    if prc.call(sorted_left.first, sorted_right.first) <= 0
       sorted << sorted_left.shift
     else
       sorted << sorted_right.shift
