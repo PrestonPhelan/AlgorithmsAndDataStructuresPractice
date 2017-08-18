3b94597e6cf21df3cf1279e6982298ed28499869
PrestonPhelan <preston.phelan@gmail.com>
2017-08-18 14:32:12 -0700

    Doubly Linked List

--- CrackingTheCodingInterview/03StacksAndQueues/3_6.py
+++ CrackingTheCodingInterview/03StacksAndQueues/3_6.py
@@ -1,65 +1,79 @@
 # Animal Shelter
-class LinkedListNode(object):
+
+### OUTLINE ####
+# Main Queue
+    # Doubly-Linked List, due to need for accessing non-first elements
+
+# Species Queues
+    # Normal queues
+#######################
+
+## Basic Class Definitions
+# Linked List node class
+class DoublyLinkedListNode(object):
     def __init__(self, value=None):
-        self.value = "HEAD" if value is None else value
+        if value is None:
+            self.value = "HEAD"
+        else:
+            self.value = value
+        self.prev = None
         self.next = None
 
     def __str__(self):
-        return self.value
+        return value
 
-    def __repr__(self):
-        return self.value
-
-class ShelterNode(LinkedListNode):
-    def __init__(self, value, species):
-        super(ShelterNode, self).__init__(value)
-        self.species = species
-
-class LinkedList(object):
+# Linked List class
+class DoublyLinkedList(object):
     def __init__(self):
-        self.head = LinkedListNode()
+        self.head = DoublyLinkedListNode()
         self.tail = self.head
 
+    def add(self, value):
+        newNode = DoublyLinkedListNode(value)
+        self.tail.next = newNode
+        newNode.prev = self.tail
+        self.tail = newNode
+
+    def remove(self, node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
+        node.next = None
+        node.prev = None
+        return node
+
+    def take(self):
+        node = self.head.next
+        self.remove(node)
+        return node
 
     def __str__(self):
-        result = []
         currentNode = self.head
-        result.append(currentNode)
+        result = ""
+        result += currentNode.value
+        result += "\n"
         while currentNode.next is not None:
             currentNode = currentNode.next
-            result.append(currentNode)
+            result += currentNode.value
+            result += "\n"
+        return result
 
-        return result.__str__()
 
-class ShelterQueue(LinkedList):
-    def enqueue(self, value, species):
-        newNode = ShelterNode(value, species)
-        self.tail.next = newNode
-        self.tail = newNode
-        return newNode
+# Stack class
 
-    def adoptAny(self):
-        if self.isEmpty():
-            print "No animals available for adoption"
-            return None
-        else:
-            toAdopt = self.head.next
-            self.head.next = toAdopt.next
-            toAdopt.next = None
-            return toAdopt
+# Queue class
 
-    def isEmpty(self):
-        return self.head.next is None
 
-x = ShelterQueue()
-x.enqueue("Rufus", "dog")
+## Shelter Classes
+# ShelterQueue
+
+# AnimalNode
+
+x = DoublyLinkedList()
+x.add("test")
+x.add("more stuff")
+
 print x
 
-print x.isEmpty()
+x.take()
 
-print x.adoptAny()
 print x
-
-print x.isEmpty()
-
-print x.adoptAny()
