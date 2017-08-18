0d70d0d4f283f4d455bf95e265c8e0bbe6b3cf62
PrestonPhelan <preston.phelan@gmail.com>
2017-08-18 12:52:04 -0700

    ShelterQueue enqueue

---
+++ CrackingTheCodingInterview/03StacksAndQueues/3_6.py
@@ -0,0 +1,44 @@
+# Animal Shelter
+class LinkedListNode(object):
+    def __init__(self, value=None):
+        self.value = "HEAD" if value is None else value
+        self.next = None
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.value
+
+class ShelterNode(LinkedListNode):
+    def __init__(self, value, species):
+        super(ShelterNode, self).__init__(value)
+        self.species = species
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = LinkedListNode()
+        self.tail = self.head
+
+
+    def __str__(self):
+        result = []
+        currentNode = self.head
+        result.append(currentNode)
+        while currentNode.next is not None:
+            currentNode = currentNode.next
+            result.append(currentNode)
+
+        return result.__str__()
+
+class ShelterQueue(LinkedList):
+    def enqueue(self, value, species):
+        newNode = ShelterNode(value, species)
+        self.tail.next = newNode
+        self.tail = newNode
+        return newNode
+
+
+x = ShelterQueue()
+x.enqueue("Rufus", "dog")
+print x
